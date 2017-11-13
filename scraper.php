584ae01e373693eb73d5ed5afcbdfbf3d25edf06
Apoorv <apoorvb3439@gmail.com>
2017-11-14 00:08:04 +0530

    Refactoring Code

--- scraper.php
+++ scraper.php
@@ -22,9 +22,14 @@ $regex="temperature";
 <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-<!-- Optional theme -->
-<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+</script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+</script>
+
 <style>
 html, body {
 height:100%;
@@ -84,16 +89,5 @@ All Copyrights Reserved &copy; Apoorv | <a href="#">About us</a> |
 </div>
 </div>
 </div>
-
-<script src="http://code.jquery.com/jquery-1.11.0.min.js">
-
-</script>
-
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js">
-
-</script>
-
 </body>
 </html>
